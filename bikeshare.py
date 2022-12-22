f22e02c780c72b2be4a3345e3bc108a777febad0
Amnah Awadh Allah Alsulami <aalsulami1996@gmail.com>
2022-12-22 03:20:16 +0300

    refactor: add display method to presents the data

--- bikeshare.py
+++ bikeshare.py
@@ -181,6 +181,8 @@ def user_stats(df, city):
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
+
+def display(df):
     
     i =1
     while True:
@@ -201,6 +203,7 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df, city)
+        display(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
